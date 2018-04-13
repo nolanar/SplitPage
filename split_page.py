86149b7c86e68845aeb517200c0f6460becb085b
Aran Nolan <nolanar@tcd.ie>
2018-04-13 16:16:11 +0100

    Add command to set the split column

--- split_page.py
+++ split_page.py
@@ -1,9 +1,10 @@
 import sublime
 import sublime_plugin
 
-class IndentToSplitCommand(sublime_plugin.TextCommand):
+SPLIT_AT_DEFAULT = 40
+_split_at = SPLIT_AT_DEFAULT
 
-    SPLIT_COL = 40
+class IndentToSplitCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         for region in self.view.sel():
@@ -11,16 +12,25 @@ class IndentToSplitCommand(sublime_plugin.TextCommand):
             begin_row, col = self.view.rowcol(text.begin())
             end_row, _ = self.view.rowcol(region.end())
 
-            if (col < self.SPLIT_COL):                    
-                insert_string = " " * (self.SPLIT_COL - col)
+            if (col < _split_at):                    
+                insert_string = " " * (_split_at - col)
                 for row in range(begin_row, end_row + 1):
                     insert_point = self.view.text_point(row, col)
                     self.view.insert(edit, insert_point, insert_string)
 
-            elif (col > self.SPLIT_COL):
-                offset = col - self.SPLIT_COL
+            elif (col > _split_at):
+                offset = col - _split_at
                 for row in range(begin_row, end_row + 1):
                     start = self.view.text_point(row, col - offset)
                     end = self.view.text_point(row, col)
                     remove = sublime.Region(start, end)
                     self.view.erase(edit, remove)
+
+class SetSplitAtCursorCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        global _split_at
+
+        for region in self.view.sel():
+            _, _split_at = self.view.rowcol(region.begin())
+            print('SplitPage: split set to col {}'.format(_split_at+1))
